4fa12d8a43ced7e9ae2e17622b76ae19d4eb0409
novatech295 <novatech295@gmail.com>
2026-01-12 15:57:42 +0300

    SDA

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,11 +3,14 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="loader-container">
-      <img
-        src="/logo.png"
-        alt="Loading"
-        className="loader-logo"
-      />
+<img
+  src="/logo.png"
+  alt="Loading"
+  className="loader-logo"
+  fetchpriority="high"
+  decoding="async"
+/>
+
 
       <div className="loader-credit en">
 Developed by NovaTech
